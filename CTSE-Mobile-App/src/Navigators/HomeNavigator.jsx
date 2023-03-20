2fd4bf371222ad2bd22e5e06081d59c6de6dec67
Kelum Ayuwardhana <kelumayuwardhana@gmail.com>
2023-03-21 00:14:12 +0530

    user management

--- CTSE-Mobile-App/src/Navigators/HomeNavigator.jsx
+++ CTSE-Mobile-App/src/Navigators/HomeNavigator.jsx
@@ -1,10 +1,6 @@
 import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "../screens/HomeScreen";
-import CustomerRegisterScreen from "../screens/Register/CustomerRegisterScreen";
-import SellerRegisterScreen from "../screens/Register/SellerRegisterScreen";
-import SelectScreen from "../screens/Register/SelectScreen";
-import Login from "../screens/Login";
 import AllUserScreen from "../screens/User Management/AllUsersScreen";
 
 const Stack = createNativeStackNavigator();
@@ -19,38 +15,6 @@ const HomeNavigator = () => {
           headerTitleAlign: "center",
         }}
         component={HomeScreen}
-      />
-      <Stack.Screen
-        name="CustomerRegisterScreen"
-        options={{
-          headerTitle: "Register",
-          headerTitleAlign: "center",
-        }}
-        component={CustomerRegisterScreen}
-      />
-      <Stack.Screen
-        name="SellerRegisterScreen"
-        options={{
-          headerTitle: "Register",
-          headerTitleAlign: "center",
-        }}
-        component={SellerRegisterScreen}
-      />
-      <Stack.Screen
-        name="SelectScreen"
-        options={{
-          headerTitle: "",
-          headerTitleAlign: "center",
-        }}
-        component={SelectScreen}
-      />
-      <Stack.Screen
-        name="LoginScreen"
-        options={{
-          headerTitle: "",
-          headerTitleAlign: "center",
-        }}
-        component={Login}
       />
       <Stack.Screen
         name="AllUserScreen"
--- CTSE-Mobile-App/src/screens/HomeScreen.jsx
+++ CTSE-Mobile-App/src/screens/HomeScreen.jsx
@@ -3,6 +3,8 @@ import React from "react";
 import { SafeAreaView } from "react-native";
 import { Button, Text } from "react-native-paper";
 import { useUserInfo } from "../services/Application";
+import { getAllUsers, getUserById } from "../services/UserService";
+
 const HomeScreen = ({ navigation }) => {
   const user = useUserInfo();
   const logout = async () => {
@@ -13,9 +15,25 @@ const HomeScreen = ({ navigation }) => {
     <SafeAreaView
       style={{ backgroundColor: "#fff", paddingHorizontal: 15, flex: 1 }}
     >
-      <Text variant="headlineMedium">{`Hello, ${user.user.firstname}`}</Text>
+      <Text variant="headlineMedium">{`Hello, ${
+        user.user.role === "customer" ? user.user.firstname : user.user.name
+      }`}</Text>
       <Button style={{ marginVertical: 30 }} onPress={logout} mode="contained">
         LogOut
+      </Button>
+      <Button
+        style={{ marginVertical: 30 }}
+        onPress={() => navigation.navigate("AllUserScreen")}
+        mode="contained"
+      >
+        All
+      </Button>
+      <Button
+        style={{ marginVertical: 30 }}
+        onPress={() => getUserById(user.id)}
+        mode="contained"
+      >
+        Test
       </Button>
     </SafeAreaView>
   );
