57f03035cc37ad18633e6808e6d558bf4022c5cb
Kelum Ayuwardhana <kelumayuwardhana@gmail.com>
2023-03-24 23:22:24 +0530

    bug fix

--- CTSE-Mobile-App/src/services/ApplicationContext.js
+++ CTSE-Mobile-App/src/services/ApplicationContext.js
@@ -12,7 +12,6 @@ const ApplicationContext = createContext();
 
 export const ApplicationProvider = (props) => {
   const [user, setUser] = useState();
-  const [payment, setPayment] = useState();
   const evts = useRef(new events.EventEmitter()).current;
 
   const getUserInfo = useCallback(async () => {
@@ -26,27 +25,10 @@ export const ApplicationProvider = (props) => {
     }
   }, []);
 
-  const getPaymentInfo = useCallback(async () => {
-    try {
-      const value = await AsyncStorage.getItem("payment");
-      const valueObject = JSON.parse(value);
-      setPayment(valueObject);
-      return valueObject;
-    } catch (e) {
-      return undefined;
-    }
-  }, []);
-
   const updateUser = async (data) => {
     await AsyncStorage.setItem("user", JSON.stringify(data));
     getUserInfo();
   };
-
-  //payment
-  const updatePaymet = async(data) => {
-    await AsyncStorage.setItem("payment", JSON.stringify(data));
-    getPaymentInfo();
-  }
 
   useEffect(() => {
     const handler = async () => {
@@ -55,27 +37,11 @@ export const ApplicationProvider = (props) => {
     handler().then();
   }, [getUserInfo]);
 
-  //payment
-  useEffect(() => {
-    const handler = async() => {
-      getPaymentInfo();
-    };
-    handler().then();
-  }, [getPaymentInfo]);
-
   const setNewUser = useMemo(
     () => ({
       updateUser,
     }),
     [updateUser]
-  );
-
-  //payment
-  const setNewPayment = useMemo(
-    () => ({
-      updatePaymet,
-    }),
-    [updatePaymet]
   );
 
   return (
@@ -84,8 +50,6 @@ export const ApplicationProvider = (props) => {
         user,
         setNewUser,
         events: evts,
-        payment,
-        setNewPayment,
       }}
     >
       {props.children}
